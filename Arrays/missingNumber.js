1a6334127777f52d471f71e83feae8d8101115f5
Jasdev2910 <jasdevvirdi987@gmail.com>
2024-12-24 06:40:28 +0530

    new problem

--- Arrays/missingNumber.js
+++ Arrays/missingNumber.js
@@ -18,3 +18,30 @@ function missingNumber(nums) {
 console.log(missingNumber(nums));
 
 //time complexity O(nlogn)
+
+// now the array has duplicates and multiple misiing numbers
+// so we can't use the above approach
+// we can use the set data structure
+
+const nums2 = [9, 6, 2, 3, 5, 7, 0, 1];
+
+function missingNumber(nums) {
+  const set = new Set(nums); // Store all numbers in a set
+  const result = []; // To store missing numbers
+  const maxNum = Math.max(...nums); // Find the maximum number in the array
+
+  console.log(set);
+
+  for (let i = 0; i <= maxNum; i++) {
+    // Loop from 0 to maxNum
+    if (!set.has(i)) {
+      result.push(i); // If the set doesn't have the number, it's missing
+    }
+  }
+
+  return result;
+}
+
+console.log(missingNumber(nums2));
+
+//time complexity O(n)
